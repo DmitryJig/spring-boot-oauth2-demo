5cfb45c6e949064bc427d24b2c97f7def4eb3d54
dima <jd2344430@gmail.com>
2023-02-20 19:47:33 +0500

    master_commit

---
+++ src/main/java/com/jd/myoauth2demo/MyOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jd.myoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOauth2DemoApplication.class, args);
+	}
+
+}
